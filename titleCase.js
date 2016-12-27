89a384b6316a16a341cd06ac8cfe5de3cd99c918
Marina Martynenko <alivemary@users.noreply.github.com>
2016-12-27 15:17:10 +0100

    Create titleCase.js

---
+++ titleCase.js
@@ -0,0 +1,17 @@
+function titleCase(str) {
+  var arr=[];
+  arr = str.split(" ");
+  for (var i=0; i<arr.length; i++){
+    
+    var j = arr[i].charAt(0).toUpperCase();
+    var array = arr[i].split("");
+    for (var k = 1; k<array.length; k++)
+      array[k] = array[k].toLowerCase();
+    var str1 = array.join("");
+    arr[i] = j + str1.substr(1);
+   
+      
+  }
+  str = arr.join(" ");
+  return str;
+}
