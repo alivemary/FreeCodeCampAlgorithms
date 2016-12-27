65f38707062fa772af95285c67653a7bf74ddd37
Marina Martynenko <alivemary@users.noreply.github.com>
2016-12-27 16:09:40 +0100

    Create truthCheck.js

---
+++ truthCheck.js
@@ -0,0 +1,25 @@
+/*
+Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+Remember, you can access object properties through either dot notation or [] notation.
+*/
+
+function truthCheck(collection, pre) {
+  // Is everyone being true?
+  var l=collection.length;
+  
+  for (var i=0; i<l; i++) {
+    if (collection[i].hasOwnProperty(pre)) {
+      if (collection[i][pre]) {
+        
+      }
+      else return false;
+    }
+    else return false;
+  }
+  
+  
+  return true;
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
