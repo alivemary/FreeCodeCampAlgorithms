85d5ffa7cd36e48fc6643733492ad91abee4869e
Marina Martynenko <alivemary@users.noreply.github.com>
2016-12-27 16:06:49 +0100

    Create dropElements.js

---
+++ dropElements.js
@@ -0,0 +1,20 @@
+/*
+Drop the elements of an array (first argument), starting from the front, until the predicate (second argument) returns true.
+
+The second argument, func, is a function you'll use to test the first elements of the array to decide if you should drop it or not.
+
+Return the rest of the array, otherwise return an empty array.
+*/
+
+function dropElements(arr, func) {
+  // drop them elements.
+  var times = arr.length;
+  for (var i = 0; i < times; i++) {
+    if (func(arr[0])) {
+      break;
+    } else {
+      arr.shift();
+    }
+  }
+  return arr;
+}
